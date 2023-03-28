8483b122cdb5d5da8b9f0f0cd3cfea41190a559f
Sonny <114654807+SonnyNA@users.noreply.github.com>
2023-03-28 15:15:39 +0000

    [+]SecretEnvironement: Adding how to use Secret from a .env

--- environ.py
+++ environ.py
@@ -1,1 +1,9 @@
+import os #Import os to allow recovery of the secret in the .env
 
+from dotenv import load_dotenv 
+
+load_dotenv() # set the load_dotenv to take the value from the .env
+
+autor_id=os.getenv('ID_AUTOR') # This is what you want to get so if you need the secret password you right in the os.getenv('PASSWORD') so switch the name variable
+
+print(autor_id) # verified print to know if the value is correct or None
